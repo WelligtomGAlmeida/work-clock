0fca23693cf646af4db9d622f35600c3efc44cea
welli <welligtomgalmeida@gmail.com>
2019-09-15 21:20:46 -0300

    Commit Inicial

---
+++ module/Empresa/src/Empresa/Form/EmpresaForm.php
@@ -0,0 +1,33 @@
+<?php
+namespace Empresa\Form;
+
+use Zend\Form\Form;
+
+class EmpresaForm extends Form
+{
+    public function __construct($name = null)
+    {
+        // Nos iremos ignorar o nome passado
+        parent::__construct('empresa');
+        $this->setAttribute('method', 'post');
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'nome',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Title',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Go',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
